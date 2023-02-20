9afb57d8a8da7ea51d2468e4b3a777f30939378a
H.J <51011169+pix3000@users.noreply.github.com>
2023-02-20 14:06:40 +0900

    Update padding.py

--- padding.py
+++ padding.py
@@ -1,21 +1,44 @@
 import numpy as np
 import PIL
 from PIL import Image
+import os
+
+Image.MAX_IMAGE_PIXELS = None
+
+def add(img, n):
+
+    dir_F = "img_old/"
+    dir_B = f"1step/BG/{n}/"
+
+    img_name  = img.split(".")[0] +'.jpg'
+
+    img = Image.open(dir_F + img)    #apis0000.jpeg
+    BG_img = Image.open(dir_B + img_name) #apis0000.jpg, 배경 이미지는 무조건 jpg로 고정
+
+        
+    h, w = img.size
+    bh, bw = BG_img.size
+
+    BG_img.paste(img, (int((bh-h)/2), int((bw-w)/2)))
+
+    BG_img.save(f'2step/{n}/{img_name}.jpg')
 
 
-img1 = "1step/black0613.JPG"
-img2 = "1step/black0613_BG_10.jpg"
 
 
-img = Image.open(img1)
-BG_img = Image.open(img2)
 
-    
-h, w = img.size
-bh, bw = BG_img.size
+file_list = os.listdir('img_old')
 
-BG_img.paste(img, (int((bh-h)/2), int((bw-w)/2)))
+file_list_2 = []
 
-print(BG_img.size)
+for file_name in file_list:
+    if file_name[-3:] != "txt":
+        file_list_2.append(file_name)
 
-BG_img.save("2step/0613_result_10.jpg")
+
+
+for file_name in file_list_2:
+    add(file_name, 1)
+    add(file_name, 3)
+    add(file_name, 5)
+    add(file_name, 10)
