6cf39b793a77f8d8a0aa59d842943ee1741a2051
HJ Gwak <51011169+pix3000@users.noreply.github.com>
2023-06-28 16:59:42 +0900

    Update coordinate_2.py

--- coordinate_2.py
+++ coordinate_2.py
@@ -16,41 +16,44 @@ def txt_read(txtname):  #text 값 읽어오기
 
 
 def n_txt(img, n):
-
-    img1 = Image.open("/home/hj/apple/apple_data/ver2/mix/train/" + img) #원본 이미지
+    img1 = Image.open("/home/hj/apple/apple_data/ver2/mix/val/" + img) #원본 이미지
     img_name = img.split(".")[0]
     img2 = Image.open(f'/home/hj/apple/apple_data/ver2/mix/0.3/{img_name}.jpg')
     tr =  img.split(".")[0] + ".txt"
-    tr = txt_read("/home/hj/apple/apple_data/ver2/mix/train/" + tr)
-    
+    tr = txt_read("/home/hj/apple/apple_data/ver2/mix/val/" + tr)
+
     if len(tr) == 0:
         return None
-    
+
     w1, h1 = img1.size
     w2, h2 = img2.size
 
     new_txt_data = []
-    
+
     for txt_data in tr:
         txt_data = txt_data.split(" ")
-        a= float(txt_data[1])   
-        b= float(txt_data[2])   
-        c= float(txt_data[3])   
-        d= float(txt_data[4])   
+        
+        try:
+            a = float(txt_data[1])
+            b = float(txt_data[2])
+            c = float(txt_data[3])
+            d = float(txt_data[4])
+        except ValueError:
+            print(f"Invalid data in {tr} -> \"{txt_data}\". Skipping this line.")
+            continue
 
         cx = ((a * w1) + (w2-w1)/2)/w2
         cy = ((b * h1) + (h2-h1)/2)/h2
         w = ((c * w1)/w2)
         h = ((d * h1)/h2)
-        
+
         new_txt_data.append(f"{txt_data[0]} {cx:.5f} {cy:.5f} {w:.5f} {h:.5f}")
-    
+
     with open(f'/home/hj/apple/apple_data/ver2/mix/0.3/{img_name}.txt', "w") as File:
         File.write("\n".join(new_txt_data))
     File.close
 
-
-all_list = os.listdir('/home/hj/apple/apple_data/ver2/mix/train')
+all_list = os.listdir('/home/hj/apple/apple_data/ver2/mix/val')
 img_list = [i for i in all_list if "txt" not in i]
 
 for i in img_list:
